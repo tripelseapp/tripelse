c85f10b62b563a38e60b40a9022f6efbdb2ba8d0
Pol Gubau Amores <gubaupol@gmail.com>
2024-07-20 12:34:23 +0200

    chore: Refactor auth service to use userService for login

--- services/api/src/auth/auth.service.ts
+++ services/api/src/auth/auth.service.ts
@@ -1,30 +1,18 @@
-import { UserDocument, UserEntity } from '@/user/entities/user.entity';
+import { UserEntity } from '@/user/entities/user.entity';
 import { UserService } from '@/user/user.service';
 import { comparePassword } from '@/user/utils/password-utils';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 import { LoginDto } from './dto/login.dto';
-import { Model } from 'mongoose';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private userModel: Model<UserDocument>,
-
-    private readonly userService: UserService,
-    private readonly jwtService: JwtService,
-  ) {}
+  constructor(private readonly userService: UserService) {}
 
   async login(loginDto: LoginDto): Promise<UserEntity> {
     // find user if a user has loginDto.usernameOrEmail as username or email and then select and get the password
-    const user = await this.userModel
-      .findOne({
-        $or: [
-          { username: loginDto.usernameOrEmail },
-          { email: loginDto.usernameOrEmail },
-        ],
-      })
-      .select('password');
+    const user = await this.userService.findByUsernameOrEmail(
+      loginDto.usernameOrEmail,
+    );
 
     if (!user) {
       throw new HttpException(
