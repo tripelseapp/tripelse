1d146e325097b34da1a671292613c6872f2ea97e
Pol Gubau Amores <gubaupol@gmail.com>
2024-08-18 22:57:00 +0200

    Refactor getUsers to getTrips for improved clarity and consistency

--- frontends/web/src/app/page.tsx
+++ frontends/web/src/app/page.tsx
@@ -3,7 +3,7 @@ import { apiCall, RequestParams } from "~/utils/apiCall";
 const endpoint = "/api/v1/trip";
 
 type GetUserOptions = RequestParams<typeof endpoint, "get">;
-async function getUsers(options: GetUserOptions = {}) {
+async function getTrips(options: GetUserOptions = {}) {
   try {
     const trips = await apiCall(endpoint, "get", options);
     return trips;
@@ -14,7 +14,7 @@ async function getUsers(options: GetUserOptions = {}) {
 }
 
 export default async function HomePage() {
-  const users = await getUsers({ page: 1, take: 2, search: "test" });
+  const users = await getTrips({ page: 1, take: 2 });
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
