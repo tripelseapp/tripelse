2efebc40e6ea4111800205cf7468329c83301c7a
Pol Gubau Amores <gubaupol@gmail.com>
2024-08-24 20:56:30 +0200

    chore: Update Dockerfile and package.json configurations

--- apps/frontends/web/next.config.js
+++ apps/frontends/web/next.config.js
@@ -5,6 +5,6 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = { output: "standalone", productionBrowserSourceMaps: false };
 
 export default config;
